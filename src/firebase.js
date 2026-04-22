b2970837bad4dd159dd9ead42007985f8ef6fc57
Leticia1905 <leticiatorreshomem@hotmail.com>
2026-04-22 17:55:32 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7aTQo0taAGi9QYyV1-jr4uW-95ZtZxg",
+  apiKey: "AIzaSyC7aTQo0taAGi9QYyV1--jr4uW-95ZtZxg",
   authDomain: "aria-cred.firebaseapp.com",
   projectId: "aria-cred",
   storageBucket: "aria-cred.firebasestorage.app",
